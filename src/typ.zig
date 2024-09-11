6e413fd686abdf7ec3994bcfd1e6d8bce0961a3c
Peter Hellberg <peter@c7.se>
2024-09-11 22:06:07 +0200

    src: Add comments

--- src/typ.zig
+++ src/typ.zig
@@ -1,19 +1,23 @@
 const std = @import("std");
 
+// Send the provided message to the WebAssembly host, and returns 0.
 pub fn ok(msg: []const u8) i32 {
     send(msg);
     return 0;
 }
 
+// Send the provided message to the WebAssembly host, and returns 1.
 pub fn err(msg: []const u8) i32 {
     send(msg);
     return 1;
 }
 
+// Send the provided message to the WebAssembly host.
 pub fn send(msg: []const u8) void {
     wasm_minimal_protocol_send_result_to_host(msg.ptr, msg.len);
 }
 
+// Write input arguments into the buffer pointed to by ptr.
 pub fn in(ptr: [*]u8) void {
     wasm_minimal_protocol_write_args_to_buffer(ptr);
 }
@@ -23,10 +27,12 @@ pub fn in(ptr: [*]u8) void {
 
 const hpa = std.heap.page_allocator;
 
+// Allocate using a `std.heap.page_allocator`.
 pub fn alloc(comptime T: type, n: usize) ![]T {
     return hpa.alloc(T, n);
 }
 
+// Free allocated memory from the `std.heap.page_allocator`.
 pub fn free(memory: anytype) void {
     hpa.free(memory);
 }
